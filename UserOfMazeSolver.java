d1ef913971b59d422cfbf9f77eca4d25d85d1c04
stevenTan01 <34069744+stevenTan01@users.noreply.github.com>
2018-04-13 00:05:46 -0400

    Update UserOfMazeSolver.java

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,7 +1,7 @@
 /**
  * Test the MazeSolver class
  *
- *Contains an instance of a maze called test0
+ * Contains an instance of a maze called test0
  *
  * test0 is used to instantiate mazeSolver tester0
  *
