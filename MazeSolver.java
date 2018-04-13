2cd88cb5628309c5fee40310dd896d01adb2df0c
stevenTan01 <34069744+stevenTan01@users.noreply.github.com>
2018-04-12 23:35:29 -0400

    added header back

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,3 +1,11 @@
+/**
+ * MazeSolver Class
+ * contains fields inProgress; type Maze
+ * contains constructor; MazeSolver(Maze maze); inProgress assigned a reference to new object of Maze
+ * contains methods solve(); 
+ * @param none
+ * @return boolean value; true means maze has been solved explorer has landed on treasure; false means maze is unsolved, possibly unsolvable
+ */
 public class MazeSolver {
 
   Maze inProgress;
