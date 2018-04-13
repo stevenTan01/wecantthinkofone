ef935d304304a38296ad9e4785bd445e34ed0ac9
Jabir Chowdhury <jchowdhury@stuy.edu>
2018-04-12 22:58:14 -0400

    Add files via upload

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,26 +1,37 @@
-/**
- * MazeSolver Class
- * contains fields inProgress; type Maze
- * contains constructor; MazeSolver(Maze maze); inProgress assigned a reference to new object of Maze
- * contains methods solve(); 
- * @param none
- * @return boolean value; true means maze has been solved explorer has landed on treasure; false means maze is unsolved, possibly unsolvable
-
- */
-
 public class MazeSolver {
 
-	Maze inProgress;
+  Maze inProgress;
+  int[] directions;
+  int[] drops;
 
-	public MazeSolver(Maze maze){
-	      inProgress = new Maze( maze);
-	      
-	}
+  public MazeSolver(Maze maze){
+    inProgress = new Maze(maze);
+    directions = new int[] { Maze.NORTH, Maze.EAST, Maze.SOUTH, Maze.WEST};
+    drops = new int[] {Maze.TREASURE, Maze.WALL, Maze.STEPPING_STONE};
+  }
 
-	public boolean solve(){
+
+  public boolean solve(){
+
 		//base cases
+
 		if(inProgress.explorerIsOnA() == Maze.WALL) return false;
+
 		else if(inProgress.explorerIsOnA() == Maze.TREASURE) return true;
-		else return false;
-	}
+
+		else {
+      Maze snapshot = new Maze(inProgress);
+      for(int dir: directions){
+        if (inProgress.checkMaybeGo(dir) == false) continue;
+        else{
+        System.out.println("in");
+        inProgress.dropA(drops[1]);
+        inProgress.go(dir);
+        if (this.solve() == true) return true;
+        inProgress = new Maze(snapshot);
+            }
+                              }
+         }
+    return false;
+  	}
 }
