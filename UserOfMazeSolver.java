f9ac427be45fa641f3aaa087fc0268c07813414e
Jabir Chowdhury <jchowdhury@stuy.edu>
2018-04-12 23:28:26 -0400

    Add files via upload

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,14 +1,19 @@
 public class UserOfMazeSolver {
 
-	public static void main(String[] commandLine)
- 	throws java.io.FileNotFoundException {
-		Maze maze = new Maze( commandLine[0]
-                            , Integer.parseInt( commandLine[1])
-                            , Integer.parseInt( commandLine[2])
-                            );
+    public static void main(String[]  commandLine)
+    throws java.io.FileNotFoundException {
+      Maze test0 = new Maze( commandLine[0]
+                          , Integer.parseInt( commandLine[1])
+                          , Integer.parseInt( commandLine[2])
+                          );
+      MazeSolver tester0 = new MazeSolver(test0);
+      System.out.println(tester0.solve());
 
-		MazeSolver solution = new MazeSolver(maze);
-		solution.solve();
-		
-	}
+
+    }
+
+
+
+
+
 }
